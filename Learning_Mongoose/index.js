f6cb1effde8d451cfdc8b4224c8c2832b135ebd3
YatharthKumarSaxena <yatharthsaxena25@gmail.com>
2025-06-04 14:06:50 +0530

    Learnt how to fetch data form MogoDB by using Mongoose, learnt how to fetch single record / multiple records using find , findOne,findByID, where clause etc.

--- Learning_Mongoose/index.js
+++ Learning_Mongoose/index.js
@@ -17,7 +17,28 @@ db.once("open",()=>{
     console.log("Connected to Mongo DB Successfully\n");
     // Logic to Insert Data into Database
     init();
+    // Logic to Retrieve Data 
+    fetchData();
 })
+
+async function fetchData(){
+    // Logic to Retrieve Data By ID
+    try{// To fetch Only Single Record // Retuen null if Record Not Found
+        const student = await StudentModel.findOne({name: "Yatarth"}); 
+        // To Fetch all Records but Reurn an Empty List [] if No Record Found
+        const teacher = await TeacherModel.find({name: "Arn"}); 
+        // Fetch Data based on Multiple Conditions
+        const teacher1 = await TeacherModel.where("name").equals("Arun").where("age").lt(90).where("age").gt(20).limit(2);
+        console.log(student);
+        console.log(teacher);
+        console.log(teacher1);
+    }
+    catch(err){
+        console.log("Error happened in Retrieving the Data");
+        console.log(err);
+    }
+    
+}
 
 async function init(){ // Function is made Async as We are using await 
     // Logic to Insert Data
@@ -34,6 +55,6 @@ async function init(){ // Function is made Async as We are using await
     }
     // const std_obj = await StudentModel.create(Student);
     // console.log(std_obj);
-    const tea_obj = await TeacherModel.create(Teacher);
-    console.log(tea_obj);
+    // const tea_obj = await TeacherModel.create(Teacher);
+    // console.log(tea_obj);
 }
